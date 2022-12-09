0a468c4475a42a171325d5b50d17ac5ed730495d
tallguy188 <cksghd4220@naver.com>
2022-12-09 16:45:42 +0900

    writing test code

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,12 +1,21 @@
 package hello.hellospring.repository;
-
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-class MemoryMemberRepositoryTest {
+import java.util.List;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
 
-    MemberRepository repository = new MemoryMemberRepository();
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // 한메서드가 끝날때마다 동작
+    public void afterEach(){
+        repository.clearStore();
+    }
+    // 메서드가 끝날때마다 데이터를 clear해주는 function
 
     @Test
     public void save() {
@@ -16,6 +25,39 @@ class MemoryMemberRepositoryTest {
         repository.save(member);
         Member result = repository.findById(member.getId()).get(); // optional의 값을 꺼낼때는 get을 써준다.
         //Assertions.assertEquals(member,result);
-        Assertions.assertThat(member).isEqualTo(result);
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result =repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+       List<Member> result = repository.findAll();
+
+       assertThat(result.size()).isEqualTo(2);
+
+
     }
 }
+// 테스트가 한개 끝날때마다 데이터를 clear해줘야됨.
